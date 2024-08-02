2403ed1b2064b8e7bcc15f79019a30ea4807fa53
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-08-02 14:36:51 +0800

    Set up readme and test code

---
+++ README.md
@@ -0,0 +1,6 @@
+Set up venv in folder containing src:
+python3 -m venv
+source env/bin/activate
+
+Install requests:
+pip install requests
--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ headers = {'Content-Type' : 'application/json',
 payload = {'operationName' : 'LocationBySearchTerm',
            'variables' : {'fuel' : 1, 'maxAge' : 0, 'search' : '92057'},
         "query": "query LocationBySearchTerm($brandId: Int, $cursor: String, $maxAge: Int, $search: String) { locationBySearchTerm(search: $search) { stations(brandId: $brandId, cursor: $cursor, maxAge: $maxAge) { count cursor { next __typename } results { fuels id name prices { cash { nickname postedTime price __typename } credit { nickname postedTime price __typename } } } __typename } trends { areaName country today todayLow trend __typename } __typename }}"}
+
 response = requests.post(url=url, headers = headers, json = payload)
 data = response.content
 
@@ -20,3 +21,27 @@ with open("data.json", "w") as outfile:
     json.dump(j, outfile)
 
 print(response.content)
+
+
+station_payload = {
+    "operationName": "GetStation",
+      "variables": {
+        "id": "31927"
+      },
+      "query": "query GetStation($id: ID!) { station(id: $id) prices { credit { nickname postedTime price } } }"
+}
+
+station_response = requests.post(url=url, headers=headers, json = station_payload)
+station_data = station_response.content
+
+text = station_data.decode(encoding = 'utf-8')
+
+json_data = json.loads(text)
+
+with open("station_data.json", "w") as outfile:
+    json.dump(json_data, outfile)
+
+print(station_response.content)
+
+
+
