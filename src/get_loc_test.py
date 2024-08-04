b10453ffdf2eef9fa412ddc679f258465609553f
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-08-04 21:13:22 +0800

    nominatim api implementation of geocoding

--- src/get_loc_test.py
+++ src/get_loc_test.py
@@ -1,32 +1,48 @@
-from geopy.geocoders import Nominatim
-from geopy.extra.rate_limiter import RateLimiter
 import requests
 import json
 from stations import GasStation
+
+def geocode_addr_zip(address: str, zipcode: str) -> tuple[str, str]:
+    payload = {"addressdetails": 1, "street": address, "postalcode": zipcode, "format": "jsonv2", "limit": 1}
+    headers = {"Referer": "testing"}
+
+    r = requests.get("https://nominatim.openstreetmap.org/search", params=payload, headers=headers)
+    data = r.content
+    text = data.decode(encoding="utf-8")
+    j = json.loads(text)[0]
+
+    coords = (j['lat'], j['lon'])
+    return coords
 
 with open('data_by_city.json', 'r') as infile:
     j = json.load(infile)
 
 GasStation.get_stations_from_json(j['data']['locationByArea']['stations'])
 
-for station in GasStation.sort_by_cheapest(GasStation.REGULAR):
+"""for station in GasStation.sort_by_cheapest(GasStation.REGULAR):
     address = station._address
     zipcode = station._zip_code.split("-")[0]
-    print(address)
+    print(station._name + " " + address)
 
-    geolocator = Nominatim(user_agent="test")
-    geocode = RateLimiter(lambda query: geolocator.geocode(f"%s, {station._region}" % query), min_delay_seconds=1)
-    location = geocode(address)
-    coords = (location.latitude, location.longitude)
+    coords = geocode_addr_zip(address, zipcode)
 
     print(coords)
-
+"""
 print('\n\n')
+
 with open('data_by_zipcode.json', 'r') as infile:
     j = json.load(infile)
 
 GasStation.get_stations_from_json(j['data']['locationBySearchTerm']['stations'])
 
 for station in GasStation.sort_by_cheapest(GasStation.REGULAR):
-    address = station._address
-    print(address)
+    address = station._name + " " + station._address
+    zipcode = station._zip_code.split("-")[0]
+    print(station._name + " " + address)
+
+    coords = geocode_addr_zip(address, zipcode)
+
+    print(coords)
+
+
+
