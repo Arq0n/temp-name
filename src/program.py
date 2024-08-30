110eb831a22c3789243e4288fe9f1434538a83c8
arq0n <71539626+Arq0n@users.noreply.github.com>
2024-08-30 23:06:04 +0800

    Refactor calculating costs to get to each station

--- src/program.py
+++ src/program.py
@@ -41,21 +41,10 @@ class Program:
         gas_station_distances = dict()
 
         for station in GasStation.sort_by_cheapest(GasStation.REGULAR):
-            station_lat, station_long = station._coords
-            dest_long_lat = f'{station_long},{station_lat}'
-
-            osrm_url = f"https://router.project-osrm.org/route/v1/driving/{self._start_long_lat};{dest_long_lat}"
-            route_json = json.loads(self._handle_request(osrm_url, HEADERS))
-
-            d = Route()
-            d.get_stats_from_json(route_json)
-            d.switch_to_imperial()
-
-            price_to_get_there = d._distance * float(station._regular.cash) / self._mpg
-            full_cost = price_to_get_there + self._gallons_to_full * float(station._regular.cash)
+            price_to_get_there, full_cost = self._calculate_costs(station, GasStation.REGULAR)
             print(f'{station.__str__()} price to get there: {price_to_get_there}; full cost: {full_cost}')
-
             gas_station_distances[station.__str__()] = full_cost
+
         print('\n\n')
         gas_station_distances = dict(sorted(gas_station_distances.items(), key=lambda item: item[1]))
 
@@ -88,6 +77,28 @@ class Program:
         """
         response = requests.post(url=url, headers=headers, json=json)
         return response.content.decode(encoding = 'utf-8')
+
+    def _calculate_costs(self, station: GasStation, gas_type: GasStation.DIESEL | GasStation.REGULAR | \
+                            GasStation.MIDGRADE | GasStation.PREMIUM) -> tuple[float, float]:
+        """
+        Calculate the costs to get to the station provided
+        Returns the price to get there and the full cost to fill the tank in a tuple
+        """
+        station_lat, station_long = station._coords
+        dest_long_lat = f'{station_long},{station_lat}'
+
+        osrm_url = f"https://router.project-osrm.org/route/v1/driving/{self._start_long_lat};{dest_long_lat}"
+        route_json = json.loads(self._handle_request(osrm_url, HEADERS))
+
+        d = Route()
+        d.get_stats_from_json(route_json)
+        d.switch_to_imperial()
+        
+        #implement calculations depending on gas types
+        #implement on whether cash or card has better value
+        price_to_get_there = d._distance * float(station._regular.cash) / self._mpg
+        full_cost = price_to_get_there + self._gallons_to_full * float(station._regular.cash)
+        return (price_to_get_there, full_cost)
 
 
 if __name__ == '__main__':
